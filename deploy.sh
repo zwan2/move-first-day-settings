98183e5e0da876f4341b7bf7ef3bd8a59f63caae
zwan <zwan@move.is>
2019-08-19 15:02:19 +0900

    travis

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:zwan2/move-first-day-settings.git master:gh-pages
 
 cd -
